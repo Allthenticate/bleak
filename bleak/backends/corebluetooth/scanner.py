482a1af73489abdd4d8243e0573f9ee87f15db41
Carglglz <carlosgilglez@gmail.com>
2020-10-09 20:08:20 +0100

    Fix corebluetooth client issue #319 (#323)
    * Fix `self._device_info` always set to `None`
    * Keep reference to `CentralManagerDelegate` object instead of relying on undocumented `CBPeriperal.manager()` method.

--- bleak/backends/corebluetooth/scanner.py
+++ bleak/backends/corebluetooth/scanner.py
@@ -164,6 +164,7 @@ class BleakScannerCoreBluetooth(BaseBleakScanner):
                     details,
                     uuids=uuids,
                     manufacturer_data=manufacturer_data,
+                    delegate=self._manager.central_manager.delegate(),
                 )
             )
 
